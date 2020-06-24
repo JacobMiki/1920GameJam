77999132a6bb2ad4498afd86a38adc4fb1bf9205
JacobMiki <JacobMiki@users.noreply.github.com>
2020-06-24 21:24:10 +0200

    Load Game scene after button press

---
+++ 1920GameJam/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameJam1920.Assets.Scripts
+{
+    public class MenuActions : MonoBehaviour
+    {
+        public void LoadScene(string scene)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+}
