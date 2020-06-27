fb771865b8ac4438b15e39ff5d5b7d42e7aa1365
aliszka <agata.anna.debiec@gmail.com>
2020-06-27 23:47:27 +0200

    Add day summary object

---
+++ 1920GameJam/Assets/Scripts/DaySummary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam1920.Assets.Scripts
+{
+    public class DaySummary : MonoBehaviour
+    {
+        [SerializeField] private TMPro.TextMeshPro _PolacyText;
+        [SerializeField] private TMPro.TextMeshPro _BolszewicyText;
+        [SerializeField] private TMPro.TextMeshPro _PowierzchniaText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
