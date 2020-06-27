941ff15614031e73d9a4b916fd3e048de4dce363
aliszka <agata.anna.debiec@gmail.com>
2020-06-27 04:57:36 +0200

    Add calendar and basic day switching

---
+++ 1920GameJam/Assets/Scripts/DaySwitcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam1920.Assets.Scripts
+{
+    public class DaySwitcher : MonoBehaviour
+    {
+        [Serializable]
+        struct Day
+        {
+            public string date;
+        }
+
+        [SerializeField] private Day[] days;
+        [SerializeField] private TMPro.TextMeshPro calendarText;
+        private int currentDay = 0;
+
+        private void Start()
+        {
+            SetCalendarDate();
+        }
+
+        private void SetCalendarDate()
+        {
+            calendarText.text = days[currentDay].date;
+        }
+
+        public void NextDay()
+        {
+            currentDay++;
+            if(currentDay>=days.Length)
+            {
+                Debug.Log("THE END");
+                return;
+            }
+            SetCalendarDate();
+        }
+    }
+}
