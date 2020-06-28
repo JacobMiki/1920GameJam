63ab0b1811b183d5c444465c1595a166ab4894e8
aliszka <agata.anna.debiec@gmail.com>
2020-06-28 15:07:46 +0200

    Fix item dragging

--- 1920GameJam/Assets/Scripts/ItemDragging.cs
+++ 1920GameJam/Assets/Scripts/ItemDragging.cs
@@ -10,6 +10,7 @@ namespace GameJam1920.Assets.Scripts
         private Vector2 mousePosition;
         private Vector2 mousePositionInWorldSpace;
         private Vector2 heldItemOffset;
+        private Vector3 heldItemPreviousPosition;
         private Transform heldItem;
         private float topItemZIndex = 0;
 
@@ -26,7 +27,14 @@ namespace GameJam1920.Assets.Scripts
         {
             if (context.canceled)
             {
-                if(heldItem)heldItem.GetComponentInChildren<DropShadow>().ShadowOffset = new Vector3(idleShadowOffset.x,idleShadowOffset.y, heldItem.GetComponentInChildren<DropShadow>().ShadowOffset.z);
+                if (heldItem)
+                {
+                    heldItem.GetComponentInChildren<DropShadow>().ShadowOffset = new Vector3(idleShadowOffset.x, idleShadowOffset.y, heldItem.GetComponentInChildren<DropShadow>().ShadowOffset.z);
+                    if(CheckIfPositionOutOfScreen(heldItem.position))
+                    {
+                        heldItem.position = heldItemPreviousPosition;
+                    }
+                }
                 heldItem = null;
                 return;
             }
@@ -37,6 +45,7 @@ namespace GameJam1920.Assets.Scripts
                 heldItem = movableItem.transform;
                 heldItemOffset = new Vector2(mousePositionInWorldSpace.x - heldItem.position.x, mousePositionInWorldSpace.y - heldItem.position.y);
                 idleShadowOffset = heldItem.GetComponentInChildren<DropShadow>().ShadowOffset;
+                heldItemPreviousPosition = heldItem.position;
             }
         }
 
@@ -47,6 +56,18 @@ namespace GameJam1920.Assets.Scripts
                 heldItem.position = new Vector3(mousePositionInWorldSpace.x - heldItemOffset.x, mousePositionInWorldSpace.y - heldItemOffset.y, topItemZIndex);
                 heldItem.GetComponentInChildren<DropShadow>().ShadowOffset = new Vector3(draggedShadowOffset.x, draggedShadowOffset.y, heldItem.GetComponentInChildren<DropShadow>().ShadowOffset.z); ;
             }
+        }
+
+        private bool CheckIfPositionOutOfScreen(Vector2 objectPos)
+        {
+            Vector3 pos = Camera.main.WorldToViewportPoint(objectPos);
+
+            if (pos.x < 0.0) return true;
+            if (1.0 < pos.x) return true;
+            if (pos.y < 0.0) return true;
+            if (1.0 < pos.y) return true;
+
+            return false;
         }
     }
 }
